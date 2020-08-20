998f6b530c45a0f5c4b7737aae6ec61fdfec00ab
Jose Paquian <alostnight1993@outlook.com>
2020-08-19 20:16:25 -0500

    MVP

---
+++ accounts/accounts-router.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const db = require("../data/dbConfig");
+const { where, del } = require("../data/dbConfig");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
+    res.status(200).json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const [account] = await db
+      .select("*")
+      .from("accounts")
+      .where({ id })
+      .limit(1);
+    res.status(200).json(account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const accountData = req.body;
+    await db("accounts")
+      .insert(accountData)
+      .then((id) => {
+        res.status(201).json({ data: id[0] });
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const change = req.body;
+  try {
+    await db("accounts")
+      .where({ id })
+      .update(change)
+      .then((count) => {
+        if (count > 0) {
+          res.status(200).json({ change });
+        } else {
+          res.status(404).json({ message: "No account with that ID" });
+        }
+      });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+    const {id} = req.params;
+    try{
+        await db("accounts")
+        .where("id", id)
+        .del()
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json({id})
+            } else {
+                res.status(404).json('Could not delete post')
+            }
+        })
+    } catch (err) {
+        next(err);
+      }
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
+const WelcomeRouter = require("../welcome/welcome-router")
+const AccountsRouter = require('../accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
+server.use("/", WelcomeRouter)
+server.use("/api/accounts", AccountsRouter);
 
 module.exports = server;
